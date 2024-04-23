0f84fceb80c2c9333d56aa7c99330e70afbd4886
Alberto R. Ramos <rr.albertoroman@gmail.com>
2024-04-22 22:55:08 -0400

    added Post routing and enabled Eloquent

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,27 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group( ['prefix' => 'api'], function () use ($router) {
+    $router->get('posts',  ['uses' => 'PostController@showAllPosts']);
+  
+    $router->get('posts/{id}', ['uses' => 'PostController@showOnePost']);
+  
+    $router->post('posts', ['uses' => 'PostController@create']);
+  
+    $router->delete('posts/{id}', ['uses' => 'PostController@delete']);
+  
+    $router->put('posts/{id}', ['uses' => 'PostController@update']);
+  });
+
+// $router->group(['prefix' => 'api'], function () use ($router) {
+//     $router->get('authors',  ['uses' => 'AuthorController@showAllAuthors']);
+  
+//     $router->get('authors/{id}', ['uses' => 'AuthorController@showOneAuthor']);
+  
+//     $router->post('authors', ['uses' => 'AuthorController@create']);
+  
+//     $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
+  
+//     $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
+//   });
